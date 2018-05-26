29d4222c54b20f8c1617af6888690490efad1f9d
BUGOVER <manukyan.artak.21@gmail.com>
2018-05-26 22:30:08 +0400

    wefew

---
+++ src/Traits/ModelExtraTrait.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Harakiri\Traits;
+
+/**
+ * Class ModelExtraTrait
+ * @package harakiri_repository_pattern\Repository\Models\Traits
+ */
+trait ModelExtraTrait
+{
+
+}
